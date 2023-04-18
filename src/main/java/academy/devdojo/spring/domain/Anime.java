e8a9bde6285dba4a4c1f9f8026df535f72a7697b
erico <ecocaval@hotmail.com>
2023-04-18 16:13:48 -0300

    feat: mysql configs

--- src/main/java/academy/devdojo/spring/domain/Anime.java
+++ src/main/java/academy/devdojo/spring/domain/Anime.java
@@ -1,11 +1,20 @@
 package academy.devdojo.spring.domain;
 
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @AllArgsConstructor
+@Entity
+@NoArgsConstructor
 public class Anime {
+
+    @Id
+    @GeneratedValue (strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Column(length = 100, nullable = false)
     private String name;
 }
